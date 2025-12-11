8bb683c49294d6870e1c8352c6395464c7133017
Pedro Schmidt <schmidthu3@outlook.com>
2025-12-11 16:38:46 -0300

    feat: added search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "Harry Potter", release_date: "2010" },
     { id: 2, title: "The Matrix", release_date: "1998" },
     { id: 3, title: "John Wick", release_date: "2017" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
 
   return (
     <div className="home">
       <form onSubmit={handleSearch} className="search-form">
-        <input type="text" placeholder="search" className="search-input" />
+        <input
+          type="text"
+          placeholder="search"
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
         <button type="submit" className="search-button">
           Search
         </button>
