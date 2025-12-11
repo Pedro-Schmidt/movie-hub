66f1888b1140f3ff52dca4cdbd70145cc0d1787e
Pedro Schmidt <schmidthu3@outlook.com>
2025-12-11 16:14:23 -0300

    fix: rendering Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
